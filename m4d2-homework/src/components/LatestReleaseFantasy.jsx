1ffa682106b3dd8ade8c80a704b4a0dd956e31d7
WHKL-9 <64627811+WHKL-9@users.noreply.github.com>
2021-08-24 16:49:29 +0200

    did minor styling to card

--- m4d2-homework/src/components/LatestReleaseFantasy.jsx
+++ m4d2-homework/src/components/LatestReleaseFantasy.jsx
@@ -18,8 +18,8 @@ class LatestReleaseFantasy extends Component{
                             <Col xs={12} sm={6} md={4} className="mb-2" key={book.asin}>
                             <Card id={book.asin} className="wholeCard">
                                 <Card.Img variant="top" src={book.img} style={{height: "16rem" }} className="img-fluid" alt="bookimage"/>
-                                    <Card.Body>
-                                        <Card.Title id="bookTitle">{book.title}</Card.Title>
+                                    <Card.Body id="bookTitle">
+                                        <Card.Title >{book.title}</Card.Title>
                                     </Card.Body>
                                 <Card.Footer>
                                     <small className="text-muted">Price: {book.price}Eur</small>
